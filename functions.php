d5253244e81c5c9dcd48a3044a5d7b398c664f02
Ethan <e.thom.butler@gmail.com>
2017-05-22 06:18:10 -0400

    fix glob bug

--- functions.php
+++ functions.php
@@ -4,6 +4,6 @@
 // This file should never be modified.
 
 $lib   = get_stylesheet_directory();
-$files = glob($lib . '/lib/**/*.php');
+$files = array_merge(glob($lib . '/lib/*/*.php'), glob($lib . '/lib/*.php'));
 
 foreach($files as $file) require_once($file);
